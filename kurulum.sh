cf84b29c8ae0c243ac47467563585a4da48e091c
SamiYusuff <126332599+SamiYusuff@users.noreply.github.com>
2023-06-05 21:39:00 +0300

    Create kurulum.sh

---
+++ kurulum.sh
@@ -0,0 +1,5 @@
+sudo pacman -Syu openbox xfce4-panel dunst numlockx obconf lxappearance-gtk3 lxapperance-obconf-gtk3 xorg network-manager-applet bleachbit curl git file-roller inxi rsync unrar unzip wget gnome-disks rofi codium firefox filezilla telegram-desktop mpv gpick ristretto mousepad gstreamer gst-plugins-good gst-plugins-bad gst-plugins-ugly ffmpeg zramswap xdg-user-dirs --noconfirm --needed
+
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com && sudo pacman-key --lsign-key FBA220DFC880C036 && sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+cd /tmp && git clone https://aur.archlinux.org/yay-git.git && cd yay-git && makepkg -si && sudo systemctl enable zramswap.service && xdg-user-dirs-update
